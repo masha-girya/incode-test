2e33aac23abbdfa094a1d876fcb82c19cc0f7257
masha-girya <teenderrr@gmail.com>
2024-01-12 13:32:06 +0200

    final

--- packages/frontend/src/constants/endpoints.constants.ts
+++ packages/frontend/src/constants/endpoints.constants.ts
@@ -1,5 +1,5 @@
-export const API_ENDPOINT = 'http://localhost:3333';
-// export const API_ENDPOINT = 'https://incode-tesst.onrender.com';
+// export const API_ENDPOINT = 'http://localhost:3333';
+export const API_ENDPOINT = 'https://incode-tesst.onrender.com';
 
 export const API_ENDPOINT_ROUTES = {
   BOARD: 'board',
